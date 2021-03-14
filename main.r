75f576cb8dab84eab4ac2061f391469f1e768bc7
Johnny Peng <woshipjl@hotmail.com>
2021-03-14 15:33:43 +1100

    update the demo code

--- main.r
+++ main.r
@@ -4,11 +4,14 @@ library(dplyr)
 library(rlist)
 source("https://raw.githubusercontent.com/JohnnyPeng123/hierarchical_bar_chart-/master/hbar.r")
 
-data <- setDT(read.csv("C:\\Users\\woshi\\Desktop\\hbarchart\\aug_train.csv"))
+# Read in sample data file
+data <- setDT(read.csv("https://raw.githubusercontent.com/JohnnyPeng123/hierarchical_bar_chart-/master/aug_train.csv"))
 data$target <- ifelse(data$target==1,"Looking for a job","Not looking for a job")
 
-# Group data by specified groupings
+# Specify the hierarchical relationship
 groupings <- c("target","last_new_job","relevent_experience","education_level")
+
+# Generate and view the hierarchical bar chart
 chart <- hbar(data,groupings,method="count",num_variable=NULL,na.rm=T)
 chart
 
