5ec1d3cc81183601e4e79ca513575cef984e976c
Johnny Peng <woshipjl@hotmail.com>
2021-03-14 18:38:21 +1100

    added in additional arguments: left_margin, font_size, ticks_width

--- hbar.r
+++ hbar.r
@@ -54,7 +54,7 @@ hbar <- function(data,groupings,method,left_margin = 200,font_size = 15, ticks_w
   return(suppressWarnings(r2d3(data=output, script =gsub("/ 80", paste0("/ ",as.character(ticks_width)),
                                                     gsub(", 20", paste0(", ",as.character(font_size)),
                                                     gsub("left: 100", paste0("left: ",as.character(left_margin)),
-                                                          readLines("C:/Users/woshi/Desktop/hbarchart/hierarchical_bar_chart-/hbar.js")
+                                                          readLines("https://raw.githubusercontent.com/JohnnyPeng123/hierarchical_bar_chart-/master/hbar.js")
                                                     )))
                                )))
 }
